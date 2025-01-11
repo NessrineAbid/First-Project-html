824b43631063a34c7293a449016036d923db7574
NessrineAbid <abiid.nessrine@gmail.com>
2025-01-11 16:01:24 +0100

    css complet

---
+++ javascript.js
@@ -0,0 +1,36 @@
+alert("hello");
+var visitorName ; 
+//prompt("saisir votre nom"); 
+console.log(visitorName); 
+console.log(typeof(true)); 
+
+console.log(5+2);
+
+var a =5; 
+var b=7; 
+a+=b; 
+console.log(a);
+
+
+var x = "google";
+x+="." + "com";
+console.log(x);
+
+function somme (a,b) {
+    return a+b;
+}
+console.log((somme(2,6))); 
+
+
+function checkifNumber(x){
+    if(typeof x ==="number"){
+        console.log(x + "is a number");
+    }
+    else {
+        console.log(x+"is not a number");
+    }
+}
+checkifNumber("a");
+
+
+
